2821de8ef021b563134f4728ecc2fbfbb89a816e
Annamalai Natarajan <annamalai.natarajan@staff.iaccap.com>
2017-06-24 11:11:00 -0700

    Linked Lists

---
+++ LinkedList/linkedlistdup.js
@@ -0,0 +1,61 @@
+function Node(element) {
+  this.element = element;
+  this.next = null;
+}
+
+function LList() {
+  this.head = new Node("head");
+  this.find = find;
+  this.insert = insert;
+  this.display = display;
+  this.findPrevious = findPrevious;
+  this.remove = remove;
+}
+
+function find(item) {
+  var currNode = this.head;
+  while(!(currNode.element == item)){
+    currNode = currNode.next;
+  }
+  return currNode;
+}
+
+function insert(newElement, item) {
+  var current = this.find(item);
+  var newNode = new Node(newElement);
+  newNode.next = current.next;
+  current.next = newNode;
+}
+
+function display() {
+  var currNode = this.head;
+  while(!(currNode.next == null)) {
+    console.log(currNode.next.element);
+    currNode = currNode.next;
+  }
+}
+
+function findPrevious(item) {
+  var currNode = this.head;
+  while(!(currNode.next == null) && (currNode.next.element != item)) {
+    currNode = currNode.next;
+  }
+  return currNode;
+}
+
+function remove(element) {
+  var prevNode = this.findPrevious(element);
+  if(!(prevNode.next == null)) {
+    prevNode.next = prevNode.next.next;
+  }
+}
+
+var cities = new LList();
+cities.insert("Chennai", "head");
+cities.insert("Vijayawada", "Chennai");
+cities.insert("Bangalore", "Vijayawada");
+cities.insert("Hyderabad","Bangalore");
+cities.display();
+cities.remove("Hyderabad");
+console.log();
+cities.display();
