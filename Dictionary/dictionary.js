0305c30d330d758e01296ba670c1db5105cd8ab6
natarajannamalaid <nataraj.annamalaid@gmail.com>
2017-06-28 08:40:47 -0700

    Dictionary and Hashing

--- Dictionary/dictionary.js
+++ Dictionary/dictionary.js
@@ -4,6 +4,8 @@ function Dictionary() {
   this.find = find;
   this.remove = remove;
   this.showAll = showAll;
+  this.count = count;
+  this.clear = clear;
 }
 
 function add(key, value) {
@@ -19,8 +21,23 @@ function remove(key) {
 }
 
 function showAll() {
+  this.datastore = Object.keys(this.datastore).sort();
   for(var key in this.datastore) {
     console.log(key + " -> " + this.datastore[key]);
+  }
+}
+
+function count() {
+  var n = 0;
+  for(var key in this.datastore) {
+    n++;
+  }
+  return n;
+}
+
+function clear() {
+  for(var key in this.datastore) {
+    delete this.datastore[key];
   }
 }
 
@@ -30,4 +47,14 @@ pbook.add("David", "345");
 pbook.add("Cynthia", "456");
 console.log("David's extension: " + pbook.find("David"));
 pbook.remove("David");
+pbook.add("Raymond","123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+pbook.add("Mike", "723");
+pbook.add("Jennifer", "987");
+pbook.add("Danny", "012");
+pbook.add("Jonathan", "666");
 pbook.showAll();
+console.log("Number of Entries: " + pbook.count());
+pbook.clear();
+console.log("Number of Entries: " + pbook.count());
